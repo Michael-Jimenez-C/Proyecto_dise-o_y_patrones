8fafd56fb2a2f9f7f8925b73280745a7f904093d
Michael Jimenez <72152481+Michael-Jimenez-C@users.noreply.github.com>
2024-02-22 16:25:42 -0500

    base

--- FrontEnd/app/src/app/navbar.tsx
+++ FrontEnd/app/src/app/navbar.tsx
@@ -1,13 +1,19 @@
 export default function Navbar(){
-  let menu=['Home','Pricing','About','Ingresar'];
+  let menu=[
+  ['Home','/'],
+  ['Products','/products'],
+  ['Pricing', '/pricing'],
+  ['About','/about'],
+  ['Sign in','login']
+  ];
     return(
         <nav className="relative md:mx-10 mx-auto mt-8 flex justify-center">
             <ol>
               {
                 menu.map((item)=>(
-                  <li className="inline-flex " key={'menu-'+item}>
-                    <a href="" className="py-2.5 md:px-5 px-4 text-black hover:bg-green-500 select-none hover:cursor-pointer">
-                    {item}
+                  <li className="inline-flex " key={'menu-'+item[0]}>
+                    <a href={item[1]} className="py-2.5 md:px-5 px-4 text-black hover:bg-green-500 select-none hover:cursor-pointer hover:text-green-50 hover:text">
+                    {item[0]}
                     </a>
                   </li>
                 ))
--- FrontEnd/app/src/app/page.tsx
+++ FrontEnd/app/src/app/page.tsx
@@ -9,7 +9,10 @@ export default function Home() {
           <Navbar></Navbar>
          </div>
          <div>
-
+            <div className="bg-gradient-to-b from-white  via-green-100 to-green-400 h-screen w-full box-border py-24">
+              <h1 className='text-green-800 text-center mx-16 font-bold text-5xl'>Uranium<span className='text-green-500'>Ex</span></h1>
+              <h2 className='text-green-800 text-center mx-16 italic text-4xl'>El mejor software empresarial para impulsar tu negocio</h2>
+            </div>
          </div>
       </div>
     </main>
