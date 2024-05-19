7ced9c2192260cbaaa461edfe8be1e5ae05d74bc
Michael Jimenez <72152481+Michael-Jimenez-C@users.noreply.github.com>
2024-05-19 12:07:44 -0500

    corrección de un color en formulario de login

--- src/components/layouts/LoginLayout.jsx
+++ src/components/layouts/LoginLayout.jsx
@@ -1,5 +1,5 @@
 export default function LoginLayout({children}){
-    return(<div className="bg-green-500 min-h-screen flex items-center justify-center">
+    return(<div className="min-h-screen flex items-center justify-center">
     <div className="bg-white p-8 rounded-lg shadow-md w-96 shadow-sm shadow-black">
       {children}
     </div>
