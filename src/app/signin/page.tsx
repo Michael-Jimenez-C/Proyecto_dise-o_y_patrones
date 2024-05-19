4418c31611aadd5502ab4ab0bcf95a32ad5827cd
Michael Jimenez <72152481+Michael-Jimenez-C@users.noreply.github.com>
2024-05-19 11:59:13 -0500

    interfaz de registro

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -25,11 +25,19 @@ export default function Login() {
         className=" mt-1 w-full rounded-md p-1 text-green-700 border-black border-2 focus:outline-slate-400"
       />
     </label>
-    <Button className="bg-green-500 rounded-xl hover:bg-green-600 text-white" handler={()=>{}}>Iniciar Sesión</Button>
+    <label className=" mb-4">
+      <span className="text-green-700">Confirme su Contraseña:</span>
+      <input
+        type="password"
+        placeholder="Ej: 12345"
+        className=" mt-1 w-full rounded-md p-1 text-green-700 border-black border-2 focus:outline-slate-400"
+      />
+    </label>
+    <Button className="bg-green-500 rounded-xl hover:bg-green-600 text-white" handler={()=>{}}>Aceptar</Button>
     <p className="text-green-700 text-center">
-      ¿No tienes cuenta?{" "}
-      <Link href="/signin" className="font-bold">
-        Registrate aquí
+      ¿ya tienes cuenta?{" "}
+      <Link href="/login" className="font-bold">
+        Ingresa
       </Link>
     </p>
   </form></LoginLayout>
