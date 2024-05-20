18e0841f01b27034140823fdec9bbcbfb54b9b22
Michael Jimenez <72152481+Michael-Jimenez-C@users.noreply.github.com>
2024-05-19 23:27:14 -0500

    añadido el contexto en el dashboard de las tarjetas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 import Navbar from "@/components/navbar";
 import Card from "@/components/card";
-
+import {useGlobalContext} from "@/context/globalContext"
 
 export default function Dashboard() {
+  const {user, setOrg} = useGlobalContext();
   return (
     
       <div className="h-full flex flex-row justify-between items-center pt-20">
