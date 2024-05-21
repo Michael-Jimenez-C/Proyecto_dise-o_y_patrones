612ac75f9a170d85615fceb0b5cd91a5df9de1fe
SACSystem <158099157+SACSystem@users.noreply.github.com>
2024-05-20 20:20:46 -0500

    Pagos eliminados

--- src/lib/payment/PayPalStrategy.ts
+++
@@ -1,7 +0,0 @@
-import PaymentStrategy from "./PaymentStrategyInterface";
-
-export default class PayPalStrategy implements PaymentStrategy {
-    processPayment(amount: number): void {
-      console.log(`Paid ${amount} using PayPal.`);
-    }
-  }
